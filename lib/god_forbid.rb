bd03ba2b4e35a9814d86eb8d0d4b64972d806564
Mehmet Beydogan <mbbrain@gmail.com>
2015-08-17 23:27:29 +0300

    Add checking request headers and raising exception

--- lib/god_forbid.rb
+++ lib/god_forbid.rb
@@ -3,12 +3,19 @@ require "god_forbid/version"
 module GodForbid
   extend ActiveSupport::Concern
 
+  @@start_with = "Bismillahirrahmanirrahim (In the name of Allah, the beneficent the merciful)"
+  @@security_policy = "God Forbid"
+
   included do
     before_action :god_forbid!
   end
 
   def god_forbid!
-    response.headers["X-Security-Policy"] = "God Forbid"
-    response.headers["X-Start-With"] = "Bismillahirrahmanirrahim (In the name of Allah, the beneficent the merciful)"
+    response.headers["X-Security-Policy"] = @@security_policy
+    response.headers["X-Start-With"] = @@start_with
+
+    if request.headers["X-Start-With"] != @@start_with || request.headers["X-Security-Policy"] != @@security_policy
+      raise "Not secure!"
+    end
   end
 end
