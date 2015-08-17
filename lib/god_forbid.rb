e57cb3130f351b0c8c00eca9a261e3e9ee4d1c59
Mehmet Beydogan <mbbrain@gmail.com>
2015-08-17 23:22:02 +0300

    Add ActiveSupport concern, before filter and response headers

--- lib/god_forbid.rb
+++ lib/god_forbid.rb
@@ -1,5 +1,14 @@
 require "god_forbid/version"
 
 module GodForbid
+  extend ActiveSupport::Concern
 
+  included do
+    before_action :god_forbid!
+  end
+
+  def god_forbid!
+    response.headers["X-Security-Policy"] = "God Forbid"
+    response.headers["X-Start-With"] = "Bismillahirrahmanirrahim (In the name of Allah, the beneficent the merciful)"
+  end
 end
